75082ff09894c8cc26b58ba1fb1e1e305c7207a3
isis lopes <isislopes2006@gmail.com>
2023-10-09 19:01:59 -0300

    noticia [03]

--- src/pages/noticias.js
+++ src/pages/noticias.js
@@ -6,22 +6,40 @@ export const Noticias = () => {
     <section className="noticias" id="noticias">
       <Container>
         <Row>
-            <Col lg={8}>
-              <h2>Você sabia?</h2>
-              <p>A inovação é uma peça-chave para o sucesso dos pequenos empreendedores. Muitas vezes, ela não requer a criação de algo completamente novo, mas sim a capacidade de aprimorar produtos ou processos já existentes. A compreensão de que inovar não se limita a introduzir algo revolucionário, mas também a encontrar maneiras criativas de melhorar o que já está presente, é fundamental.</p>
-              <p>Nos mercados cada vez mais dinâmicos e competitivos de hoje, a inovação pode ser o fator diferenciador essencial. Os empreendedores que conseguem se destacar têm maiores chances de prosperar. Isso pode envolver a otimização de produtos existentes para atender melhor às necessidades dos clientes, a melhoria de processos para aumentar a eficiência ou até mesmo a reestruturação de modelos de negócios para se adaptar às demandas do mercado.</p>
-              <p>Muitas vezes, pequenas melhorias incrementais podem gerar impactos significativos a longo prazo. A habilidade de identificar oportunidades de inovação em seu nicho de mercado pode ser a diferença entre o sucesso e o fracasso.</p>
-              <p>Não se trata apenas de criar algo novo, mas de constantemente buscar maneiras de aprimorar o que já existe, seja em produtos ou processos, para alcançar níveis mais elevados de sucesso nos negócios.</p>
+          <Col lg={8}>
+            <h2>
+              7 filmes inspiradores para empreendedores: <br></br>lições de sucesso na tela!
+            </h2>
+            <p>
+              {" "}
+              Os filmes têm o poder mágico de nos transportar para mundos
+              emocionantes, despertar paixões e inspirar empreendimentos na vida
+              real. Em homenagem ao Dia do Empreendedor, que cai no dia 5 de
+              outubro, reunimos uma lista imperdível com sete filmes repletos de
+              lições valiosas para quem sonha em construir seu próprio império.
+              Confira!
+            </p>
+            <p>
+              <ol>
+                <li>A Rede Social (2010)</li>
+                <li>Joy: O Nome do Sucesso (2015)</li>
+                <li>À procura da Felicidade (2006)</li>
+                <li>Steve Jobs (2015)</li>
+                <li>O Menino que Descobriu o Vento (2019)</li>
+                <li>Walt antes de Mickey (2015)</li>
+                <li>Fome de Poder (2016)</li>
+              </ol>
+            </p>
 
-              {/* <div className="text-center">
+            {/* <div className="text-center">
                 <a href="" target="_blank">
                   <button type="button" class="btn btn-light">saiba mais</button>
                 </a>
               </div> */}
-            </Col>
-            <Col lg={4}>
-              <img src={noticia} />
-            </Col>
+          </Col>
+          <Col lg={4}>
+            <img src={noticia} />
+          </Col>
         </Row>
       </Container>
     </section>
