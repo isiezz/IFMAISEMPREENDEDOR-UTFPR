c624df951ead1dbfd66a0247392b31e7b41ce114
isis lopes <isislopes2006@gmail.com>
2023-10-26 14:46:44 -0300

    noticias [04]

--- src/pages/noticias.js
+++ src/pages/noticias.js
@@ -8,44 +8,30 @@ export const Noticias = () => {
         <Row>
           <Col lg={8}>
             <h2>
-              O poder do diagrama de Ishikawa: uma ferramenta essencial para análise de causa e efeito.
+            Vantagens do diagrama de Ishikawa:
             </h2>
-            <p>
-            O Diagrama de Ishikawa, também chamado de diagrama de espinha de peixe ou de causa e efeito, é uma ferramenta essencial para analisar as causas de problemas em ambientes empresariais e de produção. Desenvolvido por Kaoru Ishikawa, este método se tornou fundamental para o controle de qualidade e a melhoria contínua. Ele organiza as diversas causas de um problema em categorias representadas como espinhos em uma espinha de peixe, permitindo uma análise mais aprofundada das causas e seus possíveis efeitos. Isso ajuda a identificar a causa raiz de problemas e implementar melhorias significativas. Pode ser feito da seguinte forma:
-            </p>
             <p>
               <ol>
 
-                <li>Identificando o problema:</li>
+                <li>Visão clara:</li>
                 <ul>
-                  <li>Definir claramente o problema a ser analisado. Isso é muito importante para garantir que seu diagrama de Ishikawa seja eficaz.</li>
+                  <li>Fornece uma representação visual clara das causas, facilitando a comunicação e a compreensão.</li>
                 </ul>
 
-                <li>Criando a Espinha:</li>
+                <li>Colaboração:</li>
                 <ul>
-                  <li>Desenhe uma linha horizontal para representar a espinha e rotule-a para indicar o problema identificado.</li>
+                  <li>Promove a colaboração entre os membros da equipe, fornecendo uma estrutura organizacional para discussões.</li>
                 </ul>
 
-                <li>Definindo as causas:</li>
+                <li>Identifica a causa raiz:</li>
                 <ul>
-                  <li>Desenhe linhas perpendiculares a Espinha representando as categorias das causas (pessoas, processos, equipamentos, etc.).</li>
+                  <li>Ajuda a identificar as causas raízes, permitindo soluções mais eficazes.</li>
                 </ul>
 
-                <li>Identificação de causas secundárias:</li>
+                <li>Melhoria contínua:</li>
                 <ul>
-                  <li>Para cada categoria, identifique e liste quaisquer causas secundarias que possam estar contribuindo para o problema.</li>
+                  <li>Diagramas de Ishikawa são integrados à filosofia de melhoria contínua e suportam o ciclo PDCA (Plan(Planejar), Do(Fazer), Check(Checar), Action(Agir)).</li>
                 </ul>
-
-                <li>Análise detalhada:</li>
-                <ul>
-                  <li>Explore detalhadamente cada causa secundária e utilize ferramentas como o método dos 5 Porquês para aprofundar a sua análise.</li>
-                </ul>
-
-                <li>Priorize:</li>
-                <ul>
-                  <li>Priorize as causas mais impactantes que precisam ser abordadas, considerando os recursos disponíveis e o potencial impacto positivo caso essas causas sejam solucionadas.</li>
-                </ul>
-
               </ol>
             </p>
 
